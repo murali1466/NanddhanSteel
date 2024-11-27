66590fc819da6e103cfd2eb8e15b0043931dd47d
danger-3003 <ytprogamer1213@gmail.com>
2024-11-27 15:44:23 +0530

    added patterns

--- src/Pages/ProductsPage.jsx
+++ src/Pages/ProductsPage.jsx
@@ -3,6 +3,8 @@ import Roofs from "../assets/Products/Image2.png"
 import Swiper from "../Components/Swiper"
 import AOS from "aos"
 import "aos/dist/aos.css"
+import DotPattern from "../assets/About/DotGrid.svg"
+import LinePattern from "../assets/About/LinePattern.svg"
 
 function ProductsPage() {
     useEffect(()=>{
@@ -18,7 +20,10 @@ function ProductsPage() {
             <div data-aos="fade-right" className="mt-5 text-center  px-5 md:px-7 lg:px-10">
                 <p className="font-medium text-lg ">Explore Our Wide Range of Roofing and Cladding Solutions</p>
             </div>
-            <div className="flex items-center justify-center flex-col md:flex-row gap-2 md:gap-5 pt-5 pb-10  px-5 md:px-7 lg:px-10">
+            <div className="flex items-center justify-center flex-col md:flex-row gap-2 md:gap-5 pt-5 pb-10  px-5 md:px-7 lg:px-10 relative">
+                <div>
+                    <img src={DotPattern} alt="DotGrid" className="absolute" />
+                </div>
                 <div className="flex items-start justify-center flex-col gap-2">
                     <div data-aos="fade-right" className="flex items-center justify-start bg-[#1e4b8c] p-1 rounded-3xl text-white gap-3 w-full">
                         <div className="flex items-center justify-center w-10 h-10 bg-[#cc2020] rounded-3xl font-semibold text-[#fff]"><p className="w-10 text-center">1</p></div>
@@ -67,13 +72,16 @@ function ProductsPage() {
             <div data-aos="fade" className="pb-10 flex items-center justify-center">
                 <Swiper />
             </div>
-            <div className="py-10 bg-[#f5f5f5] w-full flex items-center justify-center flex-col">
+            <div className="py-10 bg-[#f5f5f5] w-full flex items-center justify-center flex-col relative">
                 <p data-aos="fade-right" className="mb-5 font-extrabold font-[Nunito] text-2xl md:text-3xl lg:text-4xl text-[#1e4b8c] text-center">Our Roofing Accessories</p>
-                <div data-aos="fade-left" className="relative">
+                <div data-aos="fade-left" className="relative z-[2] rounded-2xl">
                     <div className="absolute mt-5 w-10 h-10 bg-[#c0212e] text-center flex items-center justify-center rounded-3xl text-white">
                         <p>9</p>
                     </div>
                     <img src={Roofs} alt="RoofingAccessories" className="w-[90vw] sm:w-[30rem] lg:w-[40rem] py-5 rounded-3xl"/>
+                </div>
+                <div>
+                    <img src={LinePattern} alt="LinePattern" className="absolute h-32 bottom-0" />
                 </div>
             </div>
         </div>
